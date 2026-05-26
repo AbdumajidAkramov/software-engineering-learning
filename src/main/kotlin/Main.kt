3fdce7c18773a50ebf4569f592d939aa4f64a7a3
a.akramov <a.akramov@hlsys.uz>
2026-05-26 18:22:02 +0500

    Software engineering kursi bo'yicha Task1 bajarildi.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,14 +3,17 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    var hamyon = 1000000 // Foydalanuvchi balansi (so'mda)
+    val mahsulotNarxi = 450000 // Sotib olinayotgan mahsulot narxi
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    // 1. Agar pul yetarli bo'lsa: hamyondan pulni ayiring va muvaffaqiyatli xabarni chiqaring.
+    // 2. Agar pul yetarli bo'lmasa: ogohlantirish xabarini va qancha pul yetishmayotganini chiqaring.
+    val isEnough: Boolean = hamyon >= mahsulotNarxi
+    if (isEnough) {
+        hamyon -= mahsulotNarxi
+        println("Muvaffaqiyatli. Hamyonda $hamyon so\'m pul qoldi")
+    } else {
+        val mustBe = hamyon - mahsulotNarxi
+        println("Ogohlantirish. Hamypnda : $mustBe so\'m bo\'lishi kerak")
     }
 }
