627180a642dc7c57978768b19427ea76eb45f16d
a.akramov <a.akramov@hlsys.uz>
2026-05-26 18:40:35 +0500

    Task1 uchun tuzatish

--- src/main/kotlin/dev/akramov/sel/task1/Main.kt
+++ src/main/kotlin/dev/akramov/sel/task1/Main.kt
@@ -3,7 +3,7 @@ package org.example.dev.akramov.sel.task1
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    var hamyon = 1000000 // Foydalanuvchi balansi (so'mda)
+    var hamyon = 150000 // Foydalanuvchi balansi (so'mda)
     val mahsulotNarxi = 450000 // Sotib olinayotgan mahsulot narxi
 
     // 1. Agar pul yetarli bo'lsa: hamyondan pulni ayiring va muvaffaqiyatli xabarni chiqaring.
@@ -11,9 +11,9 @@ fun main() {
     val isEnough: Boolean = hamyon >= mahsulotNarxi
     if (isEnough) {
         hamyon -= mahsulotNarxi
-        println("Muvaffaqiyatli. Hamyonda $hamyon so\'m pul qoldi")
+        println("Muvaffaqiyatli. Hamyonda $hamyon so'm pul qoldi")
     } else {
-        val mustBe = hamyon - mahsulotNarxi
-        println("Ogohlantirish. Hamypnda : $mustBe so\'m bo\'lishi kerak")
+        val mustBe = mahsulotNarxi - hamyon
+        println("Ogohlantirish. Hamypnda : $mustBe so'm bo'lishi kerak")
     }
 }
